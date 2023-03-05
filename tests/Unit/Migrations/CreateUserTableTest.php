bbac2b125f8d8f29c7173193b47cae424e8871d5
Beny Allan <benyallan@yahoo.com.br>
2023-03-05 13:53:56 -0300

    Test: Add test to migration users

---
+++ tests/Unit/Migrations/CreateUserTableTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit\Migrations;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schema;
+use Tests\TestCase;
+
+class CreateUserTableTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testCreateUserTableMigration(): void
+    {
+        // Executa a migration
+        Artisan::call('migrate');
+
+        // Verifica se a tabela "users" existe no banco de dados
+        $this->assertTrue(Schema::hasTable('users'));
+
+        // Verifica se as colunas da tabela "users" foram criadas corretamente
+        $this->assertTrue(Schema::hasColumns('users', [
+            'id', 'name', 'email', 'email_verified_at', 'password', 'remember_token', 'created_at', 'updated_at',
+        ]));
+    }
+}
