231c559071b0808fd17a63deaf93f1ab62386111
Beny Allan <benyallan@yahoo.com.br>
2023-03-05 16:46:25 -0300

    Refactor: Fixed Master User

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Fluent;
 
@@ -13,7 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $userData = new Fluent([
+        User::create([
             'name' => 'Beny Allan',
             'email' => 'benyallan@gamil.com',
             'password' => 'teste123',
